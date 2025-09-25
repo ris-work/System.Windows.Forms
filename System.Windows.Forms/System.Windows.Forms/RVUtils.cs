3687769099d73447b56ddf7a5c51d1f2238a3952
Rishikeshan Sulochana/Lavakumar <oss@rishikeshan.com>
2025-09-25 16:15:31 +0530

    Rounding experiments

---
+++ System.Windows.Forms/System.Windows.Forms/RVUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Forms
+{
+    public static class RVUtils
+    {
+        public static System.Drawing.Drawing2D.GraphicsPath CreateRoundedRectanglePath(Rectangle rect, int cornerRadius)
+        {
+            // Create a new path
+            var path = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // To prevent an exception, the corner radius can't be larger than half the rectangle's smallest side.
+            int diameter = Math.Min(Math.Min(rect.Width, rect.Height), cornerRadius * 2);
+
+            // If the radius is 0, just return a standard rectangle path
+            if (diameter <= 0)
+            {
+                path.AddRectangle(rect);
+                return path;
+            }
+
+            // Define the rectangle for the arcs
+            RectangleF arcRect = new RectangleF(rect.Location, new Size(diameter, diameter));
+
+            // Add the arcs for each corner
+            path.AddArc(arcRect, 180, 90); // Top-left
+            arcRect.X = rect.Right - diameter;
+            path.AddArc(arcRect, 270, 90); // Top-right
+            arcRect.Y = rect.Bottom - diameter;
+            path.AddArc(arcRect, 0, 90);   // Bottom-right
+            arcRect.X = rect.Left;
+            path.AddArc(arcRect, 90, 90);  // Bottom-left
+
+            path.CloseFigure();
+            return path;
+        }
+        public static void FillRoundedRectangle(Graphics g, Brush brush, Rectangle rect, int cornerRadius)
+        {
+            using (var path = CreateRoundedRectanglePath(rect, cornerRadius))
+            {
+                g.FillPath(brush, path);
+            }
+        }
+    }
+}
